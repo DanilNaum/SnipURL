082ddb478784a3967e401e85226a104aa3cac73e
daniil.naumkin <daniil.naumkin@simpleone.ru>
2025-03-05 10:35:48 +0300

    fix

--- internal/app/transport/rest/snipendpoint/post.go
+++ internal/app/transport/rest/snipendpoint/post.go
@@ -14,9 +14,9 @@ func (l *snipEndpoint) post(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	originalUrl := string(body)
+	originalURL := string(body)
 
-	id, err := l.service.SetURL(r.Context(), originalUrl)
+	id, err := l.service.SetURL(r.Context(), originalURL)
 	if err != nil {
 		http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
 		return
